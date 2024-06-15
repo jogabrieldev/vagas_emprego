47d061e24f3098992f719edad757222850abf930
joao gabriel <joaog4030@gmail.com>
2024-06-15 11:57:56 -0300

    primeiro commit do projeto

---
+++ src/test/java/br/dos/api/job/JobApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dos.api.job;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
